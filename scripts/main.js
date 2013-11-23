f49e4e91a8cf43227d17f58c7b42d45282de6c12
Matt Cloyd <cloydster@gmail.com>
2013-11-23 14:28:30 -0500

    cleanup before adding map

--- scripts/main.js
+++ scripts/main.js
@@ -15,20 +15,20 @@ DataManager.get_topics( function (topics) {
     SelectManager.generate_options(topics) )
 })
 
-DataManager.get_tables('Transportation', function (tables) {
-  $('select#table').html(
-    SelectManager.generate_options(tables, {text: 'title', value: 'name'}) )
-})
+// DataManager.get_tables('Transportation', function (tables) {
+//   $('select#table').html(
+//     SelectManager.generate_options(tables, {text: 'title', value: 'name'}) )
+// })
 
-DataManager.get_fields({table: 'means_transportation_to_work_by_residence', callback: function (fields) {
-  $('select#field').html(
-    SelectManager.generate_options(fields, {text: 'alias', value: 'field_name'}) )
-}})
+// DataManager.get_fields({table: 'means_transportation_to_work_by_residence', callback: function (fields) {
+//   $('select#field').html(
+//     SelectManager.generate_options(fields, {text: 'alias', value: 'field_name'}) )
+// }})
 
-DataManager.get_geographies({table: 'means_transportation_to_work_by_residence', callback: function (sumlevs) {
-  $('select#geography').html(
-    SelectManager.generate_options(sumlevs, {text: 'name', value: 'key'}) )
-}})
+// DataManager.get_geographies({table: 'means_transportation_to_work_by_residence', callback: function (sumlevs) {
+//   $('select#geography').html(
+//     SelectManager.generate_options(sumlevs, {text: 'name', value: 'key'}) )
+// }})
 
 
 
